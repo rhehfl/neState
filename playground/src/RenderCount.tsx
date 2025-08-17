d1e4be5217644f4638a7d191e9935b1046b5ebd4
GuDoYoon <rhehfl418q@gmail.com>
2025-08-17 17:37:05 +0900

    리셋

--- playground/src/RenderCount.tsx
+++
@@ -1,8 +0,0 @@
-import { useContextState } from "@goooduyn/ne-state";
-import CounterStore from "./counterStore";
-
-export default function RenderCount() {
-  const count = useContextState(CounterStore.StoreContext, (s) => s.count);
-
-  return <div>{count}</div>;
-}
